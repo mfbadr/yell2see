2d7ea0e0dd4c595d4a0dc10bb57a3d4d15a6071e
Andrew Badr <andrewbadr+git@gmail.com>
2016-02-06 12:16:42 -0500

    get volume working

--- public/yell2see.js
+++ public/yell2see.js
@@ -14,25 +14,29 @@ if (! window.AudioContext) {
 navigator.getUserMedia({audio:true}, function(stream) {
     var context = new AudioContext();
     var mediaStreamSource = context.createMediaStreamSource( stream );
-    var sourceNode = context.createBufferSource();
-    var javascriptNode = context.createScriptProcessor(2048, 1, 1);
+
     // setup a analyzer
     analyser = context.createAnalyser();
     analyser.smoothingTimeConstant = 0.3;
     analyser.fftSize = 1024;
-    sourceNode.connect(analyser);
-    analyser.connect(javascriptNode);
-    
+    //var sourceNode = context.createBufferSource();
+    //sourceNode.connect(analyser);
+    var ctx = $("#canvas").get()[0].getContext("2d");
+    var gradient = ctx.createLinearGradient(0,0,0,130);
+    gradient.addColorStop(1,'#000000');
+    gradient.addColorStop(0.75,'#ff0000');
+    gradient.addColorStop(0.25,'#ffff00');
+    gradient.addColorStop(0,'#ffffff');
+    console.log("yoo");
 
-    // create a buffer source node
-    sourceNode = context.createBufferSource();
-    javascriptNode.connect(context.destination);
+    var javascriptNode = context.createScriptProcessor(2048, 1, 1);
     javascriptNode.onaudioprocess = function() {
+        console.log("onaudioprocess");
  
         // get the average, bincount is fftsize / 2
         var array =  new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var average = getAverageVolume(array)
+        var average = getAverageVolume(array);
  
         // clear the current state
         ctx.clearRect(0, 0, 60, 130);
@@ -42,9 +46,9 @@ navigator.getUserMedia({audio:true}, function(stream) {
  
         // create the meters
         ctx.fillRect(0,130-average,25,130);
-    }
- 
+    };
     function getAverageVolume(array) {
+        console.log("getAverageVolume");
         var values = 0;
         var average;
  
@@ -59,6 +63,9 @@ navigator.getUserMedia({audio:true}, function(stream) {
         return average;
     }
     
+    mediaStreamSource.connect(analyser);
+    analyser.connect(javascriptNode);
+    javascriptNode.connect(context.destination);
 
 }, function(e) { console.log("error", e);})
 
